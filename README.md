c55f6d559bd0fc7dea2bad52b762f428d30de732
1Dice <23101388+1DIce@users.noreply.github.com>
2023-02-05 20:49:27 +0100

    Change cli commands and flags

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
-# gitlab-cli
+# gitlab-flow
 Small personal project to automate my gitlab workflow using [deno](https://deno.land/) and [cliffy](https://github.com/c4spar/deno-cliffy).
 
 
 ## Installation
 
-Use [deno](https://deno.land/) to install gitlab-cli. Make sure that `$HOME/.deno` is in your PATH.
+Use [deno](https://deno.land/) to install gitlab-flow. Make sure that `$HOME/.deno` is in your PATH.
 
 ```bash
-deno install --allow-run --allow-env --allow-net --allow-read --unstable --no-check https://raw.githubusercontent.com/1DIce/gitlab-cli/main/gitlab-cli.ts
+deno install --name glflow --allow-run --allow-env --allow-net --allow-read --no-check https://raw.githubusercontent.com/1DIce/gitlab-cli/main/main.ts
 ```
 
 ## Usage
 
 Run the `--help` command to explore the available actions.
 ```bash
-gitlab-cli --help
+glflow --help
 ```
 
 ## License
--- main.ts
+++ main.ts
@@ -13,14 +13,16 @@ import { environment } from "./environment.ts";
 
 const fs = new FileSystem();
 
-async function main() {
+function initializeConfig(): void {
   const configFile = new ConfigFileReader(fs).loadConfigFile();
   if (!configFile) {
     console.error("Error: No configuration file was found!");
     Deno.exit(1);
   }
   replaceConfig(configFile);
+}
 
+async function main() {
   await new Command()
     .name(environment.binaryName)
     .version(environment.version)
@@ -35,35 +37,56 @@ async function main() {
       { global: true, default: false },
     )
     .command(
-      "publish",
-      `Uploads new changes to merge request.
-        A remote branch is created if it does not exist.
-        A merge request is created if it does not exist.
-        The merge request is marked as ready if it was a draft`,
+      "merge-request <command>",
+      new Command<{ force: boolean }>()
+        .description("Run commands on merge requests")
+        .alias("mr")
+        .command(
+          "create",
+        )
+        .alias("c")
+        .description(
+          `Uploads new changes to merge request.
+          A remote branch is created if it does not exist.
+          A merge request is created if it does not exist.
+          By default the merge request is marked as a draft,
+          `,
+        )
+        .option(
+          "-p, --publish",
+          "The merge request is marked as ready",
+          { default: false },
+        )
+        .action(
+          (params) => {
+            initializeConfig();
+            pushToMergeRequest({ draft: !params.publish, force: params.force });
+          },
+        )
+        .reset()
+        .command(
+          "file-change <file_path:string>",
+        )
+        .description(
+          "Get url to change of the provided file in the open merge request",
+        )
+        .alias("fc")
+        .action((_params, file_path) => {
+          initializeConfig();
+          stdoutRemoteFileChangeUrl(file_path);
+        })
+        .reset()
+        .command(
+          "branch",
+        )
+        .description(
+          "Get merge request target branch if merge request for the current branch exists",
+        )
+        .action((_params) => {
+          initializeConfig();
+          stdoutTargetBranch();
+        }),
     )
-    .action((params) =>
-      pushToMergeRequest({ draft: false, force: params.force })
-    )
-    .reset()
-    .command(
-      "draft",
-      `Uploads new changes to merge request.
-        A remote branch is created if it does not exist.
-        A merge request is created if it does not exist.
-        The merge request is marked as a draft`,
-    ).action((params) =>
-      pushToMergeRequest({ draft: true, force: params.force })
-    )
-    .reset()
-    .command(
-      "file-change <file_path:string>",
-      "Get url to change of the provided file in the open merge request",
-    ).action((_params, file_path) => stdoutRemoteFileChangeUrl(file_path))
-    .reset()
-    .command(
-      "target",
-      "Get merge request target branch",
-    ).action((_params) => stdoutTargetBranch())
     .parse(Deno.args);
 }
 
